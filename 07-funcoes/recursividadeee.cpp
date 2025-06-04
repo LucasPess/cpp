0b50ed2371545fa7fae038d1e3238abe0de247a8
LucasPess <lucas82930@gmail.com>
2025-06-04 16:31:14 -0300

    entendendo a logixa de fibonacci

--- 07-funcoes/recursividadeee.cpp
+++ 07-funcoes/recursividadeee.cpp
@@ -2,35 +2,36 @@
 
 using namespace std;
 
-/*void contador(int num; int cont = 0){
+void contador(int num, int cont = 0){
     cout << cont ;
     if(num > cont){
         contador(num, ++cont);
     }
 }
-*/
 
-/*int fibonacci(int n){
+
+int fibonacci(int n){
     int x = 0;
 
     x = fibonacci(n - 1) + fibonacci(n - 2);
     return x ;
 
 
-}*/
+}
 
 int fatorial(int n){
-    int x;
+    int x = 0;
     if(n >= 1){
         x *= fatorial(n--);
     }
-    return x;
-
+	cout << x << " "; 
+	
+	return 0;
 }
 
 int main(){
 	
-	cout << fatorial(8) << " ";
+	
 	
 	//entendendo a logixca de fatorial
 	
@@ -41,6 +42,22 @@ int main(){
 		cout << x << " ";
 	}
 	*/
+	
+	//entendendo  a logica do fibonacci
+	
+	int n = 7;
+	while(n >=1){
+		int x = 0;
+		if(n == 0 || n == 1 ){
+			break;
+		}
+		
+		x = (n - 1) + (n-2);
+		cout << x;
+		
+	}
+	
+	
 
     return 0;
 }
