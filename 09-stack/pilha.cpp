25281cb870c3ae2c3a6a2b4f3c6c427824daabeb
Lucas Emanuel T. Pessoa <lucas82930@gmail.com>
2025-06-07 21:05:38 -0300

    iniciando estudos sobre stack

---
+++ 09-stack/pilha.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(){
+
+    stack <string> cartas; 
+
+    cartas.push("Mago Negro"); // Adicionando cartas a pilha
+    cartas.push("Força Espelho");
+    cartas.push("Dragão Branco de Olhos Azuis"); // a carta que esta no topo da pilha eh a ultima a ser adicionada
+
+    cout << "Tamanho da pilha: " << cartas.size() << "\n"; // como descobrir o tamanho da pilha
+
+    cartas.pop(); // retira o ultimo elemento colocado na pilha ou  o elemento do topo
+
+    cout << "Tamanho da pilha: " << cartas.size() << "\n";
+
+    cout << "carta do topo: " << cartas.top() << "\n"; //imprimindo a carta do topo
+
+
+
+    
+    return 0;
+}
