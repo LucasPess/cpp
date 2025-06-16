9d8fd8ab0321635fe7239ab37146a6555b930a8e
Lucas Emanuel T. Pessoa <lucas82930@gmail.com>
2025-06-16 00:38:26 -0300

    testando o system

---
+++ (libs)/system.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+
+    system("color 46");
+    system("dir");
+    system("pause");
+    system("cls");
+
+    return 0;
+}
