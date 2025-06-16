fea78db56bef4d4a208a73a4bb18d604e07e7269
Lucas Emanuel T. Pessoa <lucas82930@gmail.com>
2025-06-15 22:22:24 -0300

    iniciando estudos sobre ponteiros

---
+++ 13-ponteiros/ponteiros.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string veiculo = "Carro";
+    string *pv; // o ponteiro deve ser do mesmo tipo da variavel que ele vai apontar
+
+    pv = &veiculo; // ponteiro PV   recebe endereco da variavel veiculo
+
+    cout << pv << '\n' << &veiculo << '\n';
+
+    *pv = "Moto"; // no endereco apontado por *pv adicione o valor "Moto"
+
+    cout <<veiculo << '\n'<< *pv;
+    
+    return 0;
+}
