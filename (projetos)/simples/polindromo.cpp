c906bcbcf0a776dce3582aaef6f65f42db18d220
LucasPess <lucas82930@gmail.com>
2025-06-16 16:39:41 -0300

    atividade simples: chegando se a palavra é um palindromo

--- (projetos)/simples/polindromo.cpp
+++ (projetos)/simples/polindromo.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
+
 using namespace std;
 
+bool polindromo(string palavraCh){
+    string copia = palavraCh;
+    string reverso = copia;
+    reverse(reverso.begin(),reverso.end());
+    return copia == reverso;
+
+}
+
 int main(){
-    string teste1 = "ARARA";
-    if(teste1 == "ARARA"){
+    string res;
+    cout << "Digite a palavra: ";
+    cin >> res; 
+
+    if(polindromo(res)){
         cout << "Parabens eh um poindromo!";
     } else{
         cout << "Nao eh um poindromo!";
